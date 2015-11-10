8e8224a0a4731cc9630eb3fdfee9125fcbefa8ad
radgeur <remy2112@hotmail.fr>
2015-11-10 15:15:02 +0100

    real update of mbr.c/h

--- mbr.c
+++ mbr.c
@@ -1,36 +1,42 @@
 #include "mbr.h"
 
-/*int load_mbr(){
-	char buffer[SECTORSIZE];
-	assert(SECTORSIZE>=sizeof(struct mbr_s));
-	read_sector(0,0,buffer);
-	memcopy(&mbr,buffer,sizeof(struct mbr_s));
-	if(mbr.mbr_magic!= MBR_MAGIC){
-		mbr.mbr_n_vol = 0;
-		mbr.mbr_magic = MBR_MAGIC;
-		return 0;
-	}
-	return 1;
-*/
-
-int sector_of_bloc(unsigned int vol, unsigned int nbloc){
-	/*s =(Fs + b)%NSECTOR PAR CYLINDRE*/
-	return 1;
+int load_mbr(){
+    char buffer[SECTORSIZE];
+    assert(SECTORSIZE>=sizeof(struct mbr_s));
+    read_sector(0,0,buffer);
+    memcopy(&mbr,buffer,sizeof(struct mbr_s));
+    if(mbr.mbr_magic!= MBR_MAGIC){
+	mbr.mbr_nb_vol = 0;
+	mbr.mbr_magic = MBR_MAGIC;
+	return 0;
+    }
+    return 1;
 }
 
-int cylinder_of_bloc(unsigned int vol, unsigned int nbloc
-	/*c=Fc+(Fs+b)/NSECTOR ¨PAR CYLINDRE*/
-	return 1;
+int sector_of_bloc(unsigned int vol, unsigned int nbbloc){
+    unsigned int s;
+    s=(mbr.mbr_vol[vol].vol_first_sector+nbbloc)%NBSECTORPERCYLINDER;
+    return s;
 }
 
-void read_bloc(unsigned int vol, unsigned int nbloc, unsigned char *buffer){
-	int c=cylinder_of_bloc(vol,nbloc);
-	int s=sector_of_bloc(vol,nbloc);
-	read_sector(c,s,buffer);
+int cylinder_of_bloc(unsigned int vol, unsigned int nbbloc) {
+    unsigned int c;
+    c=(mbr.mbr_vol[vol].vol_first_cylinder + (mbr.mbr_vol[vol].vol_first_sector+nbbloc))%NBSECTORPERCYLINDER;
+    return c;
 }
 
-void write_bloc(unsigned int vol, unsigned int nbloc, unsigned char *buffer){
-	int c=cylinder_of_bloc(vol,nbloc);
-	int s=sector_of_bloc(vol,nbloc);
-	write_sector(c,s,buffer);
+void read_bloc(unsigned int vol, unsigned int nbbloc, unsigned char *buffer){
+    assert(MAXVOL>vol);
+    /*assert()*/;
+    int c=cylinder_of_bloc(vol,nbloc);
+    int s=sector_of_bloc(vol,nbloc);
+    read_sector(c,s,buffer);
+}
+
+void write_bloc(unsigned int vol, unsigned int nbbloc, unsigned char *buffer){
+    assert(MAXVOL>vol);
+    /*assert()*/
+    int c=cylinder_of_bloc(vol,nbloc);
+    int s=sector_of_bloc(vol,nbloc);
+    write_sector(c,s,buffer);
 }
--- mbr.h
+++ mbr.h
@@ -6,30 +6,29 @@
 #include <assert.h>
 #include "drive.h"
 
-#define MAXVOL 5
-#define MBR_MAGIC 0xCAFEBABE
-#define NBSECTORPERCYLINDER 16
+#define MAXVOL = 5
+#define MBR_MAGIC = 0xCAFEBABE
 
 enum vol_type_e {STD, ANX, OTHER};
 
 struct vol_s {
-    unsigned vol_first_sector;
-    unsigned vol_first_cylinder;
-    unsigned vol_nb_bloc;
-    enum vol_type_e vol_type;
+  unsigned vol_first_sector;
+  unsigned vol_first_cylinder;
+  unsigned vol_nb_bloc;
+  enum vol_type_e type;
 };
 
 struct MBR_s{
-    struct vol_s mbr_vol[MAXVOL];
-    unsigned mbr_nb_vol;
-    unsigned mbr_magic;	
+	struct vol_s mbr_vol[MAXVOL];
+	unsigned mbr_nb_vol;
+	unsigned mbr_magic;	
 };
 
 extern int load_mbr(); 
-extern int sector_of_bloc(unsigned int vol, unsigned int nbbloc);
-extern int cylinder_of_bloc(unsigned int vol, unsigned int nbbloc);
-extern void read_bloc(unsigned int vol, unsigned int nbbloc, unsigned char *buffer);
-extern void write_bloc(unsigned int vol, unsigned int nbbloc, unsigned char *buffer);
+extern int sector_of_bloc(unsigned int vol, unsigned int nbloc);
+extern int cylinder_of_bloc(unsigned int vol, unsigned int nbloc);
+extern void read_bloc(unsigned int vol, unsigned int nbloc, unsigned char *buffer);
+extern void write_bloc(unsigned int vol, unsigned int nbloc, unsigned char *buffer);
 extern void format_bloc(unsigned int vol);
 
 #endif
